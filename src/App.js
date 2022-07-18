2a7351a0c0c0db793059a3b2802c81ac57292076
airaheta <antonioiraheta@gmail.com>
2022-07-18 13:53:41 -0300

    NavBar
    Navbar agregada

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
-
+import NavBar from './components/NavBar.js';
 function App() {
   const name = "BikeStore"
   return (
     <div className="App">
       <header className="App-header">
+        <NavBar/>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
