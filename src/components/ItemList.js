5daaf54ab86e05073ef479dc8f99f41ffa9888de
airaheta <antonioiraheta@gmail.com>
2022-07-28 01:42:56 -0300

    fix some updates
    fix some updates

--- src/components/ItemList.js
+++ src/components/ItemList.js
@@ -1,13 +1,10 @@
-import React from "react";
 
 const ItemList = ({items}) =>  {
 return (
 <div>
-    {items.map((item)=>(<Item item= {item}/>))}    
-<div/>
-
+    {items.map((item)=>(
+    <Item key = {user.name} item= {item}/>))}    
+</div>
 );
-
-
 }   
-export default Itemlist 
+export default Itemlist; 
